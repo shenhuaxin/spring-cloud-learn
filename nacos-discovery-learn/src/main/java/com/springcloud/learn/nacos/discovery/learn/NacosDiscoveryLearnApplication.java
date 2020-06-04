0e618839cda289a63331f48be373bfe6713e5c68
shenhuaxin <shenhuaxin@extracme.com>
2020-06-04 17:51:18 +0800

    提交代码

---
+++ nacos-discovery-learn/src/main/java/com/springcloud/learn/nacos/discovery/learn/NacosDiscoveryLearnApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.learn.nacos.discovery.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosDiscoveryLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosDiscoveryLearnApplication.class, args);
+    }
+
+}
---
+++ nacos-discovery-learn/src/test/java/com/springcloud/learn/nacos/discovery/learn/NacosDiscoveryLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.learn.nacos.discovery.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosDiscoveryLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
