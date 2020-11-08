c2532dad5ac0f9203bd8252c5cf7f86925fc498f
shenhuaxin <457192344@qq.com>
2020-11-08 09:38:25 +0800

    提交代码

---
+++ eureka-register-learn/src/main/java/com/springcloud/learn/eurekaregister/learn/EurekaRegisterLearnApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.learn.eurekaregister.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaRegisterLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaRegisterLearnApplication.class, args);
+	}
+
+}
---
+++ eureka-register-learn/src/test/java/com/springcloud/learn/eurekaregister/learn/EurekaRegisterLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.learn.eurekaregister.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaRegisterLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
